13814af597cc1574eb54e09097419d8c320ac614
Rushil <121330302+mxyxyz9@users.noreply.github.com>
2025-07-05 18:43:28 +0530

    Initial Commit

---
+++ Mapd/MapdApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapdApp.swift
+//  Mapd
+//
+//  Created by Pala Rushil on 7/5/25.
+//
+
+import SwiftUI
+
+@main
+struct MapdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
